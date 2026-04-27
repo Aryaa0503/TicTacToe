ed361e0e1be55b5f6edbe02e2e639293bd85e12f
Aryaa0503 <Ab6840@srmist.edu.in>
2026-04-28 01:30:28 +0530

    uc2_toss

---
+++ src/com/tictactoe/uc2_toss.java
@@ -0,0 +1,45 @@
+package com.tictactoe;
+
+import java.util.Random;
+
+public class uc2_toss {
+
+    static boolean isHumanTurn;
+    static char humanSymbol;
+    static char computerSymbol;
+
+    public static void main(String[] args) {
+        tossAndAssignSymbols();
+        displayTossResult();
+    }
+
+    // Toss logic
+    static void tossAndAssignSymbols() {
+        Random random = new Random();
+        int toss = random.nextInt(2); // 0 or 1
+
+        if (toss == 0) {
+            isHumanTurn = true;
+            humanSymbol = 'X';
+            computerSymbol = 'O';
+        } else {
+            isHumanTurn = false;
+            humanSymbol = 'O';
+            computerSymbol = 'X';
+        }
+    }
+
+    // Display result
+    static void displayTossResult() {
+        if (isHumanTurn) {
+            System.out.println("You won the toss!");
+            System.out.println("You play first.");
+        } else {
+            System.out.println("Computer won the toss!");
+            System.out.println("Computer plays first.");
+        }
+
+        System.out.println("Your symbol: " + humanSymbol);
+        System.out.println("Computer symbol: " + computerSymbol);
+    }
+}
